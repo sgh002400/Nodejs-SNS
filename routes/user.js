3c556d56b8bdcdc31951a6c003c67755ccbeb5ad
Jihwan <sgh002400@naver.com>
2021-05-11 01:47:30 +0900

    first commit

---
+++ routes/user.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const { isLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id } }); //내가 누군지를 찾고(나에 대한 객체)
+    if (user) {
+      await user.addFollowing(parseInt(req.params.id, 10));
+      res.send('success');
+    } else {
+      res.status(404).send('no user');
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
